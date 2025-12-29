69c9171c2722481e9561e635003af6f22ba4705e
Bertl-81W <78581189+Bertl-81W@users.noreply.github.com>
2025-12-28 17:51:27 -0700

    practice C#and build console app. Fix build fail

---
+++ Program.cs
@@ -0,0 +1,20 @@
+﻿
+    /*    Console.WriteLine("Hi, what is your name?");
+        string name = Console.ReadLine()!;
+
+        Console.WriteLine($"Nice to meet you, {name}! Welcome to practicing C#."); */
+
+    class Person {
+     public string Name { get; set; }
+    public int Age { get; set; }
+    public Person(string name, int age)
+    {
+        Name = name; Age = age;
+    }
+        public void Greet() => Console.WriteLine($"Yeehaw, my name is {Name} and I am {Age} years old.");
+    }
+    var person1 = new Person("Alex", 30);
+    person1.Greet();
+    var person2 = new Person("Sam", 25);    
+    person2.Greet();  
+    
